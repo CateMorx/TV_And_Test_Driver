a96633771c2919cd929eb2d1d01f17abf6628d32
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-05-16 19:35:50 +0800

    TV class with pseudocodes

---
+++ TV.py
@@ -0,0 +1,19 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 6
+# Create a Python Code for creating the Class named TV and a Test Driver program named TestTV that will create 
+#two objects from Class TV and will produce the following output:
+#Tv1's channel is 30 and volume level is 3
+#Tv2's channel is 3 and volume level is 2
+
+#Create class TV
+#Create def for attributes
+#Create def for Turn on
+#Create def for Turn off
+#Create def that retrieves the value of channel
+#Create def for setting channel value
+#Create def that retrieves the value of volume
+#Create def for setting volume value
+#Def that increases channel level by 1
+#Def that decreases channel level by 1
+#Def that increases volume level by 1
+#Def that decreases volume level by 1
