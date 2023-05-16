dba868a39c74ccccd56b2745868809cd142e4b5b
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-05-16 20:28:36 +0800

    import element for testTV, creates class Test_TV

--- Test_TV.py
+++ Test_TV.py
@@ -4,8 +4,25 @@
 #two objects from Class TV and will produce the following output:
 #Tv1's channel is 30 and volume level is 3
 #Tv2's channel is 3 and volume level is 2
-#Imports necessary modules and elements
 
+#Imports necessary modules and elements
+import tkinter as tkinter
+from tkinter import messagebox
+from TV import TV
 #Create class Test_TV
-#Def for GUI
-#Def for buttons
+class Test_TV:
+    tv1 = TV()
+    tv2 = TV()
+    #Def for GUI
+    def __init__(self, GUI):
+        self.GUI = GUI
+        GUI.title("TV Control Panel")\
+        
+        self.tv = TV()
+        
+    #Power button
+    # Channel Label and Entry
+    # Channel Up and Down Buttons
+    # Volume Label and Entry
+    # Volume Up and Down Buttons
+#Def for button functions
